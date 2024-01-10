1cba9f0ad6e55ef0c0fc2c58c804ac25ac123412
rajibhasan01 <hasan.rajib1996@gmail.com>
2024-01-10 17:29:12 +0600

    updated for test purpose

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 
-
-app = Flask(__name__)
-
+app =  Flask(__name__)
 
 @app.route('/info', methods=['GET'])
 def info():
@@ -11,4 +9,3 @@ def info():
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port='5000')
-
